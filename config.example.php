15eb433686455459e3966879fb7a6a3d54c26749
Sidney Liebrand <sidneyliebrand@gmail.com>
2016-06-27 15:19:32 +0200

    removed excess files

--- config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  $CONFIG                           = [];
-  $CONFIG['whitelabel_credentials'] = ['name' => 'example', 'password' => 'example-password'];
-  $CONFIG['user_credentials']       = ['name' => 'whitelabel@roomstyler.com', 'password' => 'example-wl-password'];
-
-?>
